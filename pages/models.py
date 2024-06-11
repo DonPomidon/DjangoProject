9876b43c24af2ccebe3a85acf75ec15eb597390d
Dmitry Kurylenko <denny.itdep@gmail.com>
2024-06-11 09:20:54 +0300

    Task 1

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    publication_date = models.DateField(blank=False)
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,
+        related_name='books',
+        related_query_name='book',
+    )
+    rating = models.FloatField(blank=True)
+
+    def __str__(self):
+        return self.title
+
