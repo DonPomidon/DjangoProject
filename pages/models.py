a70ebe216277b75f6123be50efaccee978e96a7c
Dmitry Kurylenko <denny.itdep@gmail.com>
2024-06-12 11:06:28 +0300

    Task 4

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 
 
@@ -9,16 +10,32 @@ class Author(models.Model):
         return self.name
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique_for_month='created_at')
+    created_at = models.DateField(default=datetime.now)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, unique_for_date='publication_date')
     publication_date = models.DateField(blank=False)
     author = models.OneToOneField(
         Author,
         on_delete=models.CASCADE,
-        related_name='book',
+        related_name='books',
         related_query_name='books',
     )
     rating = models.FloatField(blank=True)
+    category = models.ManyToManyField(
+        Category,
+        related_name='books',
+        related_query_name='books',
+    )
 
     class Meta:
         verbose_name = 'Book'
@@ -33,7 +50,7 @@ class Publisher(models.Model):
     name = models.CharField(max_length=100, unique=True)
     books = models.ManyToManyField(
         Book,
-        related_name='publisher',
+        related_name='publishers',
         related_query_name='publishers',
     )
 
@@ -44,11 +61,3 @@ class Publisher(models.Model):
         return self.name
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-
-    class Meta:
-        verbose_name_plural = 'Categories'
-
-    def __str__(self):
-        return self.name
